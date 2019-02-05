4e93d54cc97c2d5a9637f3ad754fc51d32f33a9c
Wes Galbraith <galbwe92@gmail.com>
2019-02-05 07:26:24 -0600

    Added two sample projects in seed_test_db

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class DefaultConfig:
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'meep', 'database', 'test.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(DefaultConfig):
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'meep', 'database', 'dev.db')}"
 
 class TestingConfig(DefaultConfig):
     TESTING = True
     BCRYPT_LOG_ROUNDS = 4
     WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'meep', 'database', 'test.db')}"
