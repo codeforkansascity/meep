27946d6a427ef5fc7acc5924c30834dec867d8a1
Wes Galbraith <galbwe92@gmail.com>
2019-02-10 12:35:57 -0600

    populated database with random data

--- database/seed_dev_db.py
+++ database/seed_dev_db.py
@@ -7,7 +7,8 @@ from faker import Faker
 from flask import current_app
 from meep import create_app
 from meep.models import db
-from meep.models import Owner, Project
+from meep.models import Owner, Project, Address, AreaOfEffect, Coordinate
+from meep.models import FuelType, Line, Radius, Site
 from config import DevelopmentConfig
 
 
@@ -23,7 +24,7 @@ def random_address(fake):
     state = random.choice(states)
     return Address(
         address=fake.street_address(),
-        city=fake.city_name(),
+        city=fake.city(),
         state=state,
         zip=fake.zipcode_in_state(state_abbr=state)
     )
@@ -31,10 +32,9 @@ def random_address(fake):
 
 def random_coordinate(fake):
     return Coordinate(
-        lat=fake.lat(),
-        long=fake.lng()
+        lat=fake.latitude(),
+        long=fake.longitude()
     )
-
 
 
 def random_fuel_type():
@@ -70,6 +70,10 @@ def random_radius():
     )
 
 
+def random_line():
+    return Line()
+
+
 def random_site():
     return Site(
         GGE_reduced=2000*random.random(),
@@ -91,16 +95,65 @@ def seed_db():
 
     with app.app_context():
         db.create_all()
-        
+
         addresses = [random_address(fake) for i in range(5)]
         coordinates = [random_coordinate(fake) for i in range(10)]
-        fuels = [random_fuel_type(fake) for i in range(5)]
+        fuels = [random_fuel_type() for i in range(5)]
         owners = [random_owner(fake) for i in range(5)]
         rads = [random_radius() for i in range(5)]
+        lines = [random_line() for i in range(5)]
         sites = [random_site() for i in range(5)]
         projects = [random_project(fake) for i in range(5)]
+        aoes = []
+
+        # add owner relationships
+        for address in addresses:
+            address.coordinate = random.choice(coordinates)
+
+        # area of effect relationships
+        for i in range(5):
+            is_radius = random.choice([True, False])
+            geom = random.choice(rads) if is_radius else random.choice(lines)
+            if is_radius:
+                aoes.append(AreaOfEffect(
+                    address = random.choice(addresses),
+                    radius = geom
+                ))
+            else:
+                aoes.append(AreaOfEffect(
+                    address = random.choice(addresses),
+                    line = geom
+                ))
+
+        # fuel type relationships
+        for ft in fuels:
+            n_aoes = random.randint(0,5)
+            ft.areas_of_effect = list(set([
+                random.choice(aoes) for i in range(n_aoes)
+            ]))
+
+        # owner relationships
+        for owner in owners:
+            n_addresses = random.randint(0,5)
+            n_projects = random.randint(0,5)
+            owner.addresses = [random.choice(addresses) for i in range(n_addresses)]
+            owner.projects = [random.choice(projects) for i in range(n_projects)]
 
 
+        #line relationships
+        for line in lines:
+            line.end_location = random.choice(addresses)
+
+
+        # project relationships
+        for project in projects:
+            project.sites = random.sample(sites, random.randint(0,5))
+
+
+        # site relationships
+        for site in sites:
+            site.address = random.choice(addresses)
+            site.areas_of_effect = random.sample(aoes, random.randint(0,3))
 
 
         for objects in [addresses, coordinates, fuels, owners, rads,
