f5cc1a6eca5e49bb8addeeba5994e51df2c0bb9d
Piero Pretto <pretto417@gmail.com>
2019-02-09 20:51:08 -0600

    unpack react to intergate with flask

---
+++ meep/static/react/precache-manifest.c3ddd50728ec4d2732b201bee8ada1e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0444190ad4b59ea0baef",
+    "url": "/static/react/js/runtime~main.0444190a.js"
+  },
+  {
+    "revision": "93c5220d10333e817dd0",
+    "url": "/static/react/js/main.93c5220d.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/react/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "93c5220d10333e817dd0",
+    "url": "/static/react/css/main.aa2e28d6.chunk.css"
+  },
+  {
+    "revision": "073666810ea94529ad3878682f9e01ab",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
