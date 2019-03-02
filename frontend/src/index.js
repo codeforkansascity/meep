0dbfa37c298bca4ae4e406b1108c2f5d55a014d1
Piero Pretto <pretto417@gmail.com>
2019-03-01 21:09:46 -0600

    config webpack

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 
-const template = React.createElement('p', {}, 'testing 123');
-ReactDOM.render(AppRouter, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
   }
 };
