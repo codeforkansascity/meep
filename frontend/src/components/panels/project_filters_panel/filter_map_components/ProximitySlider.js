a9ecf84ed0e9ae71f833dad23189221f549304d9
Wes Galbraith <galbwe92@gmail.com>
2019-05-26 12:45:48 -0500

    added slider functionality

--- frontend/src/components/panels/project_filters_panel/filter_map_components/ProximitySlider.js
+++ frontend/src/components/panels/project_filters_panel/filter_map_components/ProximitySlider.js
@@ -23,24 +23,86 @@ class ProximitySlider extends Component {
   }
 }
 
-const ProximitySliderTrack = props => {
-  var {position} = props;
-  var leftWidth = `${Math.floor(position*100)}%`;
-  var rightWidth = `${Math.floor((1 - position)*100)}%`
-  return (
-    <div className="proximity-slider-track">
+class ProximitySliderTrack extends Component {
+  constructor(props){
+    super(props);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.stopDragging = this.stopDragging.bind(this);
+    this.sliderTrackDomElement = React.createRef();
+    var {position} = props;
+    this.state = {
+      position: position,
+      leftWidth: `${Math.floor(position*100)}%`,
+      rightWidth: `${Math.floor((1 - position)*100)}%`,
+      dragging: false,
+    }
+  }
+
+  handleMouseDown(e) {
+    console.log('mouse down')
+    this.setState({
+      dragging: true,
+    })
+  }
+
+  handleMouseMove(e) {
+    if (this.state.dragging) {
+      console.log('dragging')
+      var boundingRect
+      boundingRect = this.sliderTrackDomElement.current.getBoundingClientRect()
+      var elemLeft = boundingRect.left
+      var elemRight = boundingRect.right
+      var mouseX = e.clientX
+      console.log(`elemLeft = ${elemLeft}`)
+      console.log(`elemRight = ${elemRight}`)
+      console.log(`mouseX = ${mouseX}`)
+      var position = (mouseX - elemLeft)/(elemRight - elemLeft);
+      position = position > 0 ? position : 0;
+      position = position < 1 ? position : 1;
+      console.log(`position = ${position}`)
+      this.setState({
+        position: position,
+        leftWidth: `${Math.floor(position*100)}%`,
+        rightWidth: `${Math.floor((1 - position)*100)}%`,
+      })
+
+    }
+  }
+
+  stopDragging(e) {
+    console.log('mouse up')
+    this.setState({
+      dragging: false,
+    })
+  }
+
+
+  render() {
+    return (
       <div
-        className="proximity-slider-track-left"
-        style={{width: leftWidth}}>
+        className="proximity-slider-track"
+        ref={this.sliderTrackDomElement}
+        onMouseMove={this.handleMouseMove}
+        onMouseUp={this.stopDragging}
+        onMouseLeave={this.stopDragging}>
+        <div
+          className="proximity-slider-track-left"
+          style={{width: this.state.leftWidth}}>
+        </div>
+        <div
+          className="proximity-slider-track-thumb"
+          onMouseDown={this.handleMouseDown}
+        ></div>
+        <div
+          className="proximity-slider-track-right"
+          style={{width: this.state.rightWidth}}>
+        </div>
       </div>
-      <div className="proximity-slider-track-thumb"></div>
-      <div
-        className="proximity-slider-track-right"
-        style={{width: rightWidth}}>
-      </div>
-    </div>
-  )
+    )
+  }
 }
+
 
 const ProximitySliderTicks = props => {
   return (
