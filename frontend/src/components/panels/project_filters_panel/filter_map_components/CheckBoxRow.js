cb6fc4fa335c9bf8992f5b20eedef3303516aacf
Clement Ng <clementdng@gmail.com>
2019-06-02 00:36:19 -0700

    [WIP] Added event handler so clicking the labels toggles the checkbox (but not React)

--- frontend/src/components/panels/project_filters_panel/filter_map_components/CheckBoxRow.js
+++ frontend/src/components/panels/project_filters_panel/filter_map_components/CheckBoxRow.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import ProjectTypeMarker from '../../../helpers/projectTypeMarker';
 
-const CheckBoxRow = ({ Label, ProjectType }) => (
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input"/>
-        <label title={Label} type="checkbox" class="form-check-label">{Label}</label>
-        <ProjectTypeMarker FillClass={ProjectType}/>
-    </div>
-);
+let form_check_id = 0;
+const CheckBoxRow = ({ Label, ProjectType}) => {
+    function toggleView (Label) {
+        let new_check_state = !document.getElementById(Label).getElementsByClassName("form-check-input")[0].checked;
+        // console.log(new_check_state);
+        document.getElementById(Label).getElementsByClassName("form-check-input")[0].checked = new_check_state;
+    };
+    return (
+        <div class="form-check" id={Label}>
+            <input type="checkbox" class="form-check-input"/>
+            <label title={Label} type="checkbox" class="form-check-label" onClick={() => {toggleView(Label)}}>{Label}</label>
+            <ProjectTypeMarker FillClass={ProjectType}/>
+        </div>  
+    );     
+};
 
 export default CheckBoxRow;
