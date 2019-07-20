9d8e91207ea015a9f0a2a04c737cb70224bb5466
Piero Pretto <ppretto@amplymedia.com>
2019-07-20 18:48:15 -0500

    fix class declarations

--- frontend/src/components/panels/project_filters_panel/filter_map_components/CheckBoxRow.js
+++ frontend/src/components/panels/project_filters_panel/filter_map_components/CheckBoxRow.js
@@ -3,9 +3,9 @@ import Form from 'react-bootstrap/Form';
 import ProjectTypeMarker from '../../../helpers/projectTypeMarker';
 
 const CheckBoxRow = ({ Label, ProjectType }) => (
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input"/>
-        <label title={Label} type="checkbox" class="form-check-label">{Label}</label>
+    <div className="form-check">
+        <input type="checkbox" className="form-check-input"/>
+        <label title={Label} type="checkbox" className="form-check-label">{Label}</label>
         <ProjectTypeMarker FillClass={ProjectType}/>
     </div>
 );
