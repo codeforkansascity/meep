9854e1e3e2981f9e6c58b7bce78ded8c2e9036d5
Piero Pretto <ppretto@amplymedia.com>
2019-07-27 22:01:33 -0500

    update readme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import axios from 'axios';
+
+axios.get('http://localhost:8000/locations')
+    .then((res) => {
+        console.log(res.data);
+    });
 
 //import custom sass
 import './styles/main.scss';
