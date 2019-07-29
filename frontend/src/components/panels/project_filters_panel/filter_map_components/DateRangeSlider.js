ff5f8457068956e8b0658123a31e062de5fc01a0
Matthaus Klute <klute101@gmail.com>
2019-07-29 13:11:45 -0500

    Installed package, styled range slider

--- frontend/src/components/panels/project_filters_panel/filter_map_components/DateRangeSlider.js
+++ frontend/src/components/panels/project_filters_panel/filter_map_components/DateRangeSlider.js
@@ -1,9 +1,28 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+const marks = {
+	2000: '2000',
+	2005: '2005',
+	2010: '2010',
+	2015: '2015',
+	2018: '2018',
+};
+
+const dotStyle = {
+	'bottom': '-10px',
+    'width': '1px',
+    'border': '1px solid #e9e9e9',
+    'border-radius': '0%',
+    'margin-left': '-1px',
+};
 
 const DateRangeSlider = () => (
-    <div className="my-1">
-        <input type="range" className="custom-range" id="customRange1"/>
-    </div>
+	    <div>
+	    	<Range min={2000} max={2018} marks={marks} dots={true} dotStyle={dotStyle} defaultValue={[2015, 2018]}/>
+	    </div>
 );
 
 export default DateRangeSlider;
