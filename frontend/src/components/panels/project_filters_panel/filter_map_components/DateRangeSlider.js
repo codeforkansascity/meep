84d5df04273166193aeca47380806e3c14c8b077
Matthaus Klute <klute101@gmail.com>
2019-08-12 19:56:03 -0500

    Added tooltip formatting

--- frontend/src/components/panels/project_filters_panel/filter_map_components/DateRangeSlider.js
+++ frontend/src/components/panels/project_filters_panel/filter_map_components/DateRangeSlider.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import Tooltip from 'rc-tooltip';
 import Slider, { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css';
 
-const marks = {
-	2000: '2000',
-	2005: '2005',
-	2010: '2010',
-	2015: '2015',
-	2018: '2018',
-};
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Handle = Slider.Handle;
 
 const dotStyle = {
 	'bottom': '-10px',
@@ -17,6 +14,21 @@ const dotStyle = {
     'border': '1px solid #e9e9e9',
     'border-radius': '0%',
     'margin-left': '-1px',
+};
+
+const handle = (props) => {
+  const { value, dragging, index, key, className, disabled, offset, prefixCls} = props;
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle value={value} dragging={dragging} index={index} key={key} className={className} disabled={disabled} offset={offset} prefixCls={prefixCls} />
+    </Tooltip>
+  );
 };
 
 class DateRangeSlider extends React.Component {
@@ -28,10 +40,31 @@ class DateRangeSlider extends React.Component {
 
 		}
 	}
+	marks(minValue, maxValue) {
+	  let yearLabels = [];
+	  for (let i = minValue; i <= maxValue; i++ ) {
+	    if (i == minValue || i == maxValue || i % 5 == 0) {
+	      yearLabels.push(i);
+	    }
+	  }
+	  return yearLabels.reduce(function(result, year) {
+	    result[year] = year.toString();
+	    return result;
+	  }, {});
+	}
+
 	render() {
 		return (
 		    <div>
-		    	<Range min={this.state.minValue} max={this.state.maxValue} marks={marks} dots={true} dotStyle={dotStyle} defaultValue={[2015, 2018]}/>
+		    	<Range
+		    		min={this.state.minValue}
+		    		max={this.state.maxValue}
+		    		marks={this.marks(this.state.minValue, this.state.maxValue)}
+		    		dots={true}
+		    		dotStyle={dotStyle} 
+		    		defaultValue={[2015, 2018]}
+		    		handle={handle}
+		    		/>
 		    </div>
 			)
 	}
--- frontend/src/components/panels/project_filters_panel/filter_map_components/ProximitySlider.js
+++ frontend/src/components/panels/project_filters_panel/filter_map_components/ProximitySlider.js
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import Slider, { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Slider from 'rc-slider';
+import Tooltip from 'rc-tooltip';
 
-const marks = {
-	1: '1 mi',
-	5: '5 mi',
-	10: '10 mi',
-	20: '20 mi',
-};
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Handle = Slider.Handle;
 
 const dotStyle = {
 	'bottom': '-10px',
@@ -18,6 +16,21 @@ const dotStyle = {
     'margin-left': '-1px',
 };
 
+const handle = (props) => {
+  const { value, dragging, index, key, className, disabled, offset, prefixCls} = props;
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle value={value} dragging={dragging} index={index} key={key} className={className} disabled={disabled} offset={offset} prefixCls={prefixCls} />
+    </Tooltip>
+  );
+};
+
 class ProximitySlider extends React.Component {
 	constructor(props) {
 		super(props);
@@ -25,11 +38,34 @@ class ProximitySlider extends React.Component {
 			minValue: 1,
 			maxValue: 20,
 		}
+		this.marks = this.marks.bind(this);
+	}
+	marks(minValue, maxValue) {
+		console.log(minValue);
+	  let miLabels = [];
+	  for (let i = minValue; i <= maxValue; i++ ) {
+	    if (i == minValue || i == maxValue || i % 5 == 0) {
+	      miLabels.push(i);
+	    }
+	  }
+	  return miLabels.reduce(function(result, year) {
+	    result[year] = year.toString();
+	    console.log(result);
+	    return result;
+	  }, {});
 	}
 	render() {
 		return (
 			<div>
-				<Slider min={this.state.minValue} max={this.state.maxValue} step={1} marks={marks} dotStyle={dotStyle} defaultValue={10} />
+				<Slider 
+					min={this.state.minValue}
+					max={this.state.maxValue}
+					step={1}
+					marks={this.marks(this.state.minValue, this.state.maxValue)}
+					dotStyle={dotStyle}
+					defaultValue={10}
+					handle={handle}
+				/>
 			</div>
 		);
 	}
