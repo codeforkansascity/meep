51d5eb71d1c8a52ee236077465e2a8881d90eb66
Piero Pretto <ppretto@amplymedia.com>
2019-08-17 18:12:58 -0500

    revert webpack and babel files to working build

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,31 +1,25 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    },
+    {
+      test: /\.s?css$/,
+       use: ['style-loader', 'css-loader', 'sass-loader']
+    }]
   },
-  devtool: "cheap-module-eval-source-map",
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true,
-    compress: true,
-    host: "0.0.0.0",
-    port: 8080,
-    disableHostCheck: true
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
   }
 };
