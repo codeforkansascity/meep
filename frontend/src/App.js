55f224dc4d930ab73078224ca0300d78f787ad8e
Piero Pretto <ppretto@amplymedia.com>
2019-09-21 15:42:49 -0500

    load map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { WelcomePanel, ProjectListPanel, ProjectFiltersPanel, ProjectDetailsPanel }  from './components/panels/index';
 import { Route, Switch } from 'react-router-dom';
-import {  MapContainer  } from './components/panels/map_panel/MapPanelContainer';
+import MapContainer from './components/panels/map_panel/MapPanelContainer';
 
 class App extends Component {
   render() {
--- frontend/src/components/panels/map_panel/MapPanelContainer.js
+++ frontend/src/components/panels/map_panel/MapPanelContainer.js
@@ -1,20 +1,30 @@
 import { GoogleApiWrapper, Map } from 'google-maps-react';
 import React, { Component } from 'react';
- 
+
+const mapStyles = {
+    width: '100%',
+    height: '100%',
+  };
+
 export class MapContainer extends Component {
     render() {
-        return (
-            <Map
-                google={this.props.google}
-                initialCenter={{
-                    lat: 40.854885,
-                    lng: -88.081807
-                }}
-                zoom={15}></Map>
-        )
-    }
+        if(!this.props.google) {
+            return <div ref='map'>Loading map from API...</div>
+        } else {
+            return (
+                <Map
+                    style={mapStyles}
+                    google={this.props.google}
+                    initialCenter={{
+                        lat: 40.854885,
+                        lng: -88.081807
+                    }}
+                    zoom={15}></Map>
+            )
+        }
+    } 
 }
  
 export default GoogleApiWrapper({
-    //   apiKey: <copy and paste API key here>
+    // apiKey: paste API Key here
 })(MapContainer);
