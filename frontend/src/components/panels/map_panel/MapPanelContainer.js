60cc1bf67dedf9e5d5e489e43bb2b92e4ef0c4f7
Piero Pretto <ppretto@amplymedia.com>
2019-09-21 15:04:14 -0500

    init Map Container component

---
+++ frontend/src/components/panels/map_panel/MapPanelContainer.js
@@ -0,0 +1,21 @@
+import { GoogleApiWrapper, Map } from 'google-maps-react';
+import React, { Component } from 'react';
+ 
+export class MapContainer extends Component {
+    render() {
+        return (
+            <Map
+                style={style}
+                google={this.props.google}
+                initialCenter={{
+                    lat: 40.854885,
+                    lng: -88.081807
+                }}
+                zoom={15}></Map>
+        )
+    }
+}
+ 
+export default GoogleApiWrapper({
+    //   apiKey: <copy and paste API key here>
+})(MapContainer);
