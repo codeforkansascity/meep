726f52c36d9987f7ad2ba6fc3805312d70feb800
bbradforddesign <bbradforddesign>
2019-09-23 18:51:30 -0500

    initial commit containing map with polygon

--- frontend/src/components/panels/map_panel/MapPanelContainer.js
+++ frontend/src/components/panels/map_panel/MapPanelContainer.js
@@ -1,20 +1,33 @@
-import { GoogleApiWrapper, Map } from 'google-maps-react';
+import { GoogleApiWrapper, Map, Polygon, Circle } from 'google-maps-react';
 import React, { Component } from 'react';
  
 export class MapContainer extends Component {
     render() {
+        const PolyCoords = [
+            {lat: 39.3002992, lng: -95.0017571},
+            {lat: 39.4616436, lng: -94.6008616},
+            {lat: 39.0383863, lng: -93.9446012},
+            {lat: 38.6640670, lng: -94.4059056},
+            {lat: 38.7690750, lng: -95.0347074}];
+        const ReduxPercent = .5
         return (
             <Map
                 google={this.props.google}
                 initialCenter={{
-                    lat: 40.854885,
-                    lng: -88.081807
-                }}
-                zoom={15}></Map>
+                    lat: 39.2,
+                    lng: -94.5}}
+                className={'map'}
+                zoom={8}>
+                    <Polygon
+                    path={PolyCoords}
+                    strokeColor="#0000FF"
+                    strokeOpacity={ReduxPercent}
+                    strokeWeight={2} />
+                </Map>
         )
     }
 }
  
 export default GoogleApiWrapper({
-    //   apiKey: <copy and paste API key here>
+    apiKey: 'AIzaSyC1o7P7OG08az4tlbCjLwN-u16HKAR2t3Q'
 })(MapContainer);
