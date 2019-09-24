17779550d50e435e90d016c15202a82f6df446a8
bbradforddesign <bbradforddesign>
2019-09-23 22:01:41 -0500

    switched library to google-map-react for shape support

--- frontend/src/components/panels/map_panel/MapPanelContainer.js
+++ frontend/src/components/panels/map_panel/MapPanelContainer.js
@@ -1,33 +1,64 @@
-import { GoogleApiWrapper, Map, Polygon, Circle } from 'google-maps-react';
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
  
-export class MapContainer extends Component {
-    render() {
-        const PolyCoords = [
-            {lat: 39.3002992, lng: -95.0017571},
-            {lat: 39.4616436, lng: -94.6008616},
-            {lat: 39.0383863, lng: -93.9446012},
-            {lat: 38.6640670, lng: -94.4059056},
-            {lat: 38.7690750, lng: -95.0347074}];
-        const ReduxPercent = .5
-        return (
-            <Map
-                google={this.props.google}
-                initialCenter={{
-                    lat: 39.2,
-                    lng: -94.5}}
-                className={'map'}
-                zoom={8}>
-                    <Polygon
-                    path={PolyCoords}
-                    strokeColor="#0000FF"
-                    strokeOpacity={ReduxPercent}
-                    strokeWeight={2} />
-                </Map>
-        )
-    }
+export default class MapContainer extends Component {
+
+  //create props for map component  
+  static defaultProps = {
+    center: {
+      lat: 39.1,
+      lng: -94.6
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+        //container for map. height is required to render
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+                //store API key here
+                bootstrapURLKeys={{ key: 'api key here' }}
+                //reference map component props here
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+                //create new polygons, circles, etc on top of map once loaded
+                //utilize Google Maps api to create shapes rather than rely on library components
+                onGoogleApiLoaded={({map}) => {
+                    //new circle
+                    new google.maps.Circle({
+                        //styling
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 0.8,
+                        strokeWeight: 2,
+                        fillColor: '#FF0000',
+                        fillOpacity: 0.3,
+                        //render circle onto map
+                        map,
+                        //receive props from map
+                        center: this.props.center,
+                        //set circle radius
+                        radius: 5000,
+                    })
+                    //new polygon
+                    new google.maps.Polygon({
+                        //polygon edges
+                        paths: [{lat: 39, lng: -94.5},
+                            {lat: 39.1, lng: -94.6},
+                            {lat: 39, lng: -94.6}],
+                        //styling
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 0.8,
+                        strokeWeight: 2,
+                        fillColor: '#FF0000',
+                        fillOpacity: 0.35,
+                        //render polygon onto map
+                        map
+                    })
+                }}
+            >
+            </GoogleMapReact>
+        </div>
+    );
+  }
 }
- 
-export default GoogleApiWrapper({
-    apiKey: //api key here
-})(MapContainer);
