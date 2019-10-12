293ca8f60e6d5721a34c6efb1fca0115633924f8
Piero Pretto <ppretto@amplymedia.com>
2019-10-12 16:47:22 -0500

    handle conflicts

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     {
       test: /\.s?css$/,
-       use: ['style-loader', 'css-loader', 'sass-loader']
+       use: ['style-loader','css-loader', 'sass-loader']
     }]
   },
   devtool: 'cheap-module-eval-source-map',
